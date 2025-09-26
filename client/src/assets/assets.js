69498086c0d3dce8ecc43ba02fb3f9c67dc7465b
Gnaht <rexming2016@gmail.com>
2025-09-26 23:52:55 +0700

    Add Home page and Hero component with assets
    Introduces a new Home page and Hero component, updates App.jsx to use React Router for routing, and adds new SVG and image assets for UI elements. Also updates the assets.js export to include new icons.

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,8 +1,12 @@
 // Import all your SVG files
 import Logo from './favicon.svg';
-
+import SearchIcon from './search.svg';
+import MenuIcon from './menuIcon.svg';
+import CloseIcon from './closeIcon.svg';
 // Export them for easy import elsewhere
 export default {
     Logo,
-
+    SearchIcon,
+    MenuIcon,
+    CloseIcon,
 };
---
+++ client/src/component/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='flex flex-col items-start justify-center px-6 md:px-16 
+        1g:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.jpg")] bg-cover bg-center bg-no-repeat h-screen'>
+
+        </div>
+    )
+}
+
+export default Hero
