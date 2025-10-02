a9680bf622dbe3508071d35e10dbae9bd46f688f
Gnaht <rexming2016@gmail.com>
2025-10-02 21:59:49 +0700

    Create check in & check out form

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -83,11 +83,55 @@ const RoomDetails = () => {
                 </div>
                 {/* Room Price */}
                 <div className="mt-4 md:mt-0 md:ml-8 flex-shrink-0">
-                    <p className='text-2xl font-medium'>
+                    <p className='text-3xl font-medium'>
                         ${room.pricePerNight} <span className="text-base font-normal text-gray-500">/ night</span>
                     </p>
                 </div>
             </div>
+
+            {/*Check in & out form*/}
+            <form className='flex flex-col md:flex-row items-start md:items-center
+justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-xl mx-auto mt-16 max-w-6xl'>
+                <div className='flex flex-col flex-wrap md:flex-row items-start md:items-center gap-4 md:gap-10 text-gray-500'>
+
+                    <div className='flex flex-col'>
+                        <label htmlFor='checkInDate' className='font-medium'>Check in</label>
+                        <input type='date' id='checkInDate' placeholder='Check in'
+                            className='w-full rounded border border-gray-200 px-3 py-2 mt-1.5 outline-none' required />
+                    </div>
+
+                    <div className='w-px h-15 bg-gray-300/70 max-hd:hidden'></div>
+
+                    <div className='flex flex-col'>
+                        <label htmlFor='checkOutDate' className='font-medium'>Check Out</label>
+                        <input type='date' id='checkOutDate' placeholder='Check out'
+                            className='w-full rounded border border-gray-200 px-3 py-2 mt-1.5 outline-none' required />
+                    </div>
+
+                    <div className='w-px h-15 bg-gray-300/70 max-hd:hidden'>
+
+                    </div>
+
+                    <div className='flex flex-col'>
+                        <label htmlFor='guests' className='font-medium'>Guests</label>
+                        <input type='number' id='guests' placeholder='0' min='1'
+                            className='max-w-20 rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required />
+                    </div>
+
+                </div>
+
+                <button type='submit' className='bg-blue-500 hover:bg-blue-600 active:scale-95 
+                transition-all text-white rounded-md max-md:w-full max-md:mt-6 md:px-6 py-3 md:py-4 text-base 
+                cursor-pointer font-medium'>
+                    Check Availability
+                </button>
+            </form>
+
+            {/*Common Specifications*/}
+            <div>
+                {room}
+            </div>
+
         </div>
     )
 }
