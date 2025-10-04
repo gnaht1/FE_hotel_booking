70b9a4476a16b8407d00c4e21ed36786155ae1b2
Gnaht <rexming2016@gmail.com>
2025-10-04 17:39:48 +0700

    Add hotel owner dashboard and room pages
    Introduced Dashboard, AddRoom, and ListRoom components for hotel owners. Updated routing in App.jsx to support nested owner routes for managing rooms and viewing dashboard.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import RoomDetails from './pages/RoomDetails'
 import MyBookings from './pages/MyBookings';
 import HotelReg from './component/HotelReg';
 import Layout from './pages/HotelOwner/Layout'
+import Dashboard from './pages/HotelOwner/Dashboard'
+import AddRoom from './pages/HotelOwner/AddRoom'
+import ListRoom from './pages/HotelOwner/ListRoom'
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes('owner');
@@ -22,7 +25,12 @@ const App = () => {
           <Route path='/rooms' element={<AllRooms />} />
           <Route path='/rooms/:id' element={<RoomDetails />} />
           <Route path='/my-bookings' element={<MyBookings />} />
-          <Route path='/owner' element={<Layout />} />
+          <Route path='/owner' element={<Layout />} >
+            <Route index element={<Dashboard />} />
+            <Route path='add-room' element={<AddRoom />} />
+            <Route path='list-room' element={<ListRoom />} />
+          </Route>
+          
         </Routes>
       </div>
       <Footer />
---
+++ client/src/pages/HotelOwner/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Title from '../../component/Title'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Title 
+        align='left' font='outfit' title='Dashboard' 
+        subTitle='Monitor your room listings, track bookings and analyze revenue—all in one place. Stay updated with real-time insights to ensure smooth operations.' 
+      />
+    </div>
+  )
+}
+
+export default Dashboard
