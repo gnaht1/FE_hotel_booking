731c38a6f345e3602f964e6c78efd5fd4bb41915
Gnaht <rexming2016@gmail.com>
2025-10-13 23:50:09 +0700

    phase 1

--- client/src/component/HotelReg.jsx
+++ client/src/component/HotelReg.jsx
@@ -1,41 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import assets from '../assets/assets'
 import { cities } from '../assets/assets'
 import { useAppContext } from '../context/AppContext'
+import { toast } from 'react-hot-toast'
 
 const HotelReg = () => {
-    const {setShowHotelReg} = useAppContext();
+    const {setShowHotelReg, axios, getToken, setIsOwner} = useAppContext();
+
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [contact, setContact] = useState('');
+    const [city, setCity] = useState('');
+
+    const onSubmitHandler = async (e)=>{
+        try {
+            e.preventDefault();
+            const data = await axios.post(`/api/hotels`, {name, contact, address, city}, 
+                {headers: {Authorization: `Bearer ${await getToken()}`}})
+
+            if(data.success){
+                toast.success(data.message);
+                setIsOwner(true);
+                setShowHotelReg(false);
+            }
+            else {
+                toast.error(data.message); 
+            }
+        } catch (error) {
+            toast.error(error.message); 
+        }
+    }
+
   return (
-    <div className='fixed top-0 bottom-0 left-0 right-0 z-50 flex items-center justify-center bg-black/70'>
-        <form className='flex bg-white rounded-xl max-w-4xl max-md:mx-2 relative'>
+    <div onClick={()=> setShowHotelReg(false)} className='fixed top-0 bottom-0 left-0 right-0 z-50 flex items-center justify-center bg-black/70'>
+        <form onSubmit={onSubmitHandler} onClick={(e)=> e.stopPropagation()}
+        className='flex bg-white rounded-xl max-w-4xl max-md:mx-2 relative'>
             <img src={assets.regImage} alt="reg-image" className='w-1/2 rounded-l-xl hidden md:block'/>
             
             <div className='relative flex flex-col items-center md:w-1/2 p-8 md:p-10'>
-                <img src={assets.CloseIcon} alt="close-icon" className='absolute top-4 right-4 h-4 w-4 cursor-pointer' onClick={()=> setShowHotelReg(false)}/>
+                <img src={assets.CloseIcon} alt="close-icon" className='absolute top-4 right-4 h-4 w-4 cursor-pointer' 
+                onClick={()=> setShowHotelReg(false)}/>
                 <p className='text-2xl font-semibold mb-6'>Register Your Hotel</p>
 
                 {/* Hotel Name */}
                 <div className='w-full mt-4'>
                     <label htmlFor="name" className='font-medium text-gray-500'>Hotel Name</label>
-                    <input type="text" id="name" placeholder='Enter Hotel Name' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required />
+                    <input type="text" id="name" 
+                    onChange={(e)=> setName(e.target.value)} value={name}
+                    placeholder='Enter Hotel Name' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required />
                 </div>
 
                 {/*Phone*/}
                 <div className='w-full mt-4'>
                     <label htmlFor="contact" className='font-medium text-gray-500'>Phone</label>
-                    <input type="text" id="contact" placeholder='Enter Phone Number' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required />
+                    <input type="text" id="contact"
+                    onChange={(e)=> setContact(e.target.value)} value={contact}
+                     placeholder='Enter Phone Number' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required />
                 </div>
 
                 {/*Address*/}
                 <div className='w-full mt-4'>
                     <label htmlFor="address" className='font-medium text-gray-500'>Address</label>
-                    <input type="text" id="address" placeholder='Enter Address' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required />
+                    <input type="text" id="address"
+                    onChange={(e)=> setAddress(e.target.value)} value={address}
+                     placeholder='Enter Address' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required />
                 </div>
 
                 {/*Select cities*/ }
                 <div className='w-full mt-4 max-w-60 mr-auto'>
                     <label htmlFor="city" className='font-medium text-gray-500'>City</label>
-                    <select id='city' className='border border-gray-200
+                    <select id='city' 
+                    onChange={(e)=> setCity(e.target.value)} value={city}
+                    className='border border-gray-200
                     rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required>
                         {cities.map((city, i) => (
                             <option key={i} value={city}>{city}</option>
