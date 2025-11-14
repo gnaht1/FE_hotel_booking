0b78a3eec5b3ae30f1abdc7f091aea341d8d5bf9
Gnaht <rexming2016@gmail.com>
2025-11-14 16:40:58 +0700

    fix section format

--- client/src/component/FeaturedDestination.jsx
+++ client/src/component/FeaturedDestination.jsx
@@ -15,10 +15,12 @@ const FeaturedDestination = () => {
             <Title title='Featured Destination' subTitle='Discover our handpicked selection of exceptional 
             properties around the world, offering unparalleled luxury and unforgettable experiences.'/>
 
-            <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
-                {rooms.slice(0, 4).map((room, index) => (
-                    <HotelCard key={room._id} room={room} index={index} />
-                ))}
+            <div className='mt-20 w-full max-w-6xl mx-auto overflow-x-auto'>
+                <div className='flex flex-nowrap items-stretch justify-center gap-6 min-w-fit'>
+                    {rooms.slice(0, 4).map((room, index) => (
+                        <HotelCard key={room._id} room={room} index={index} />
+                    ))}
+                </div>
             </div>
 
             <button onClick={() => { navigate('/rooms'); scrollTo(0, 0) }}
--- client/src/component/RecommendedHotels.jsx
+++ client/src/component/RecommendedHotels.jsx
@@ -26,10 +26,12 @@ const RecommendedHotels = () => {
             of exceptional properties around the world, offering unparalleled luxury and 
             unforgettable experiences.'/>
 
-            <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
-                {recommended.slice(0,4).map((room, index)=>(
-                    <HotelCard key={room._id} room={room} index={index}/>
-                ))}
+            <div className='mt-20 w-full max-w-6xl mx-auto overflow-x-auto'>
+                <div className='flex flex-nowrap items-stretch justify-center gap-6 min-w-fit'>
+                    {recommended.slice(0,4).map((room, index)=>(
+                        <HotelCard key={room._id} room={room} index={index}/>
+                    ))}
+                </div>
             </div>
         </div>
     )
